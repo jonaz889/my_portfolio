b6d8af0839970099cc8d6d47efe187f91b8444ff
Jonas H. Møller <jonasmoller8713@gmail.com>
2026-07-07 01:55:14 +0200

    Complete Astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://jonasmoeller.netlify.app"
+  site: "https://jonasmoeller.netlify.app",
+  integrations: [preact()]
 });
